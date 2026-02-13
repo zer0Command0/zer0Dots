ba40d2034ca2a5bd14404d493770cb3870aaefe7
alessioHoboh <alessio@hoboh.net>
2026-02-13 21:52:10 +0100

    chore: refresh Brewfile and sync chezmoi config updates

--- dot_config/sketchybar/plugins/executable_cpu.sh
+++ dot_config/sketchybar/plugins/executable_cpu.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-CPU=$(ps -A -o %cpu | awk '{s+=$1} END {printf "%.0f", s/8}')
-sketchybar --set "$NAME" label="${CPU}%"
+source "$HOME/.config/sketchybar/colors.sh"
+
+CORE_COUNT=$(sysctl -n hw.ncpu)
+CPU_PERCENT=$(ps -A -o %cpu | awk -v cores="$CORE_COUNT" 'NR>1 {sum += $1} END {printf "%.0f", sum / cores}')
+[ -z "$CPU_PERCENT" ] && CPU_PERCENT=0
+
+COLOR=$SUBTEXT
+case "$CPU_PERCENT" in
+[3-5][0-9]) COLOR=$ACCENT_MID ;;
+[6-7][0-9]) COLOR=$ACCENT_STRONG ;;
+[8-9][0-9] | 100) COLOR=$RED ;;
+esac
+
+sketchybar --set cpu.percent label="$CPU_PERCENT%" label.color="$COLOR"
